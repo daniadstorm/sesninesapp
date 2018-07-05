ef8d2e457932e7eec380686eb184fe087320f461
sergio-adstorm <sergio.vegas@adstormcommunication.com>
2018-07-05 14:06:44 +0200

    Sergio
    Modelo de inputs
    Integrar libreria componente upload img

--- nueva-categoria.php
+++ nueva-categoria.php
@@ -3,9 +3,11 @@ include_once('config/config.inc.php'); //cargando archivo de configuracion
 $uM = load_model('usuario');
 $uM->control_sesion($ruta_inicio, ADMIN);
 
-$fM = load_model('form');
 $hM = load_model('html');
+$iM = load_model('inputs');
 $catM = load_model('categorias');
+
+$ttl = 'Nueva categoría';
 
 //campos formulario
 $id_categoria = 0;
@@ -15,13 +17,28 @@ $imagen_categoria = '';
 $arr_err = array();
 $verif = true;
 
+//GET___________________________________________________________________________
+if (isset($_GET['id_categoria'])) {
+    
+    $id_categoria = $_GET['id_categoria'];
+    
+    $rgc = $catM->get_categoria($id_categoria);
+    
+    if ($rgc) {
+        while ($fgc = $rgc->fetch_assoc()) {
+            $nombre_categoria = $fgc['nombre_categoria'];
+            $imagen_categoria = $fgc['imagen_categoria'];
+        }
+    } else $str_errores = $hM->get_alert_danger('Error cargando categoría');
+}
+//GET___________________________________________________________________________
+
 //POST__________________________________________________________________________
 if (isset($_POST['id_categoria'])) {
     
     $id_categoria = $_POST['id_categoria'];
     $nombre_categoria = $_POST['nombre_categoria'];
     $imagen_categoria = $_POST['imagen_categoria'];
-    
     
     //control de errores ---------------------------------------------------- */
     //control de errores ---------------------------------------------------- */
@@ -51,29 +68,11 @@ if (isset($_POST['id_categoria'])) {
 }
 //POST__________________________________________________________________________
 
-//GET___________________________________________________________________________
-if (isset($_REQUEST['id_categoria'])) {
-    
-    $id_categoria = $_REQUEST['id_categoria'];
-    
-    $rgctr = $catM->get_categoria();
-    
-    if ($rgu) {
-        while ($fgu = $rgu->fetch_assoc()) {
-            $nombrecompleto_usuario = $fgu['nombrecompleto_usuario'];
-            $email_usuario = $fgu['email_usuario'];
-            $fecha_nacimiento = $fgu['fecha_nacimiento'];
-            $contrasenya_usuario = $fgu['contrasenya_usuario'];
-            $telf_usuario = $fgu['telf_usuario'];
-            $nie_usuario = $fgu['nie_usuario'];
-        }
-    } else $str_errores = $str_errores = '<div class="error_alert">Error cargando usuario</div>';
-}
-//GET___________________________________________________________________________
-
 //COMBOS________________________________________________________________________
 
 //COMBOS________________________________________________________________________
+
+if ($id_categoria > 0) $ttl = 'Editar categoría';
 
 include_once('inc/cabecera.inc.php'); //cargando cabecera 
 ?>
@@ -98,18 +97,27 @@ include_once('inc/cabecera.inc.php'); //cargando cabecera
                                     <h4>Nuevo cliente</h4>
                                 </div>
                                 <div class="layout-table-content">
-                                    <form action="nuevo-cliente.php" method="post">
-                                        hey
-                                        <input pattern=".{3,}"   required title="3 characters minimum john boy">
+                                    <form method="post">
                                     <?php 
-                                        echo $fM->get_input_hidden('id_usuario', $id_usuario);
-                                        echo $fM->get_input_text('nombrecompleto_usuario', 'Nombre completo', $nombrecompleto_usuario, $arr_err);
-                                        echo $fM->get_input_text('email_usuario', 'Email', $email_usuario, $arr_err);
-                                        echo $fM->get_input_date('fecha_nacimiento', 'Fecha de nacimiento', $fecha_nacimiento, $arr_err);
-                                        echo $fM->get_input_text('contrasenya_usuario', 'Contraseña', $contrasenya_usuario, $arr_err);
-                                        echo $fM->get_input_text('telf_usuario', 'Telefono', $telf_usuario, $arr_err);
-                                        echo $fM->get_input_text('nie_usuario', 'DNI / NIE', $nie_usuario, $arr_err);
+                                        echo $iM->get_input_hidden('id_categoria', $id_categoria);
+                                        echo $iM->get_input_text('nombre_categoria', $nombre_categoria, 'form-control', 'Nombre categoría', '', 'Campo requerido', 1);
                                     ?>
+                                    <div class="form-group">
+                                        <label>Imagen categoría</label>
+                                        <div class="file-loading">
+                                            <input id="file-es" name="file-es[]" type="file" multiple>
+                                        </div>
+                                    </div>
+                                    <script>
+                                        $('#file-es').fileinput({
+                                            theme: 'fa',
+                                            language: 'es',
+                                            //uploadUrl: '#',
+                                            showUpload: false,
+                                            allowedFileExtensions: ['jpg', 'png', 'gif']
+                                        });
+                                    </script>
+                                    
                                     <button class="btn bg-primary text-light">Aceptar</button>
                                     </form>
                                 </div>
@@ -119,43 +127,6 @@ include_once('inc/cabecera.inc.php'); //cargando cabecera
                 </div>
             </div>
         </div>
-<!-- <div id="main_container">
-    <div id="responsive_back_content">
-        <?php include_once('inc/franja_top.inc.php'); ?>
-        <?php include_once('inc/main_menu.inc.php'); ?>
-        <div id="responsive_seccion_back">
-            
-            <div id="ttl_seccion_back">
-                <h2>Nuevo Usuario</h2>
-                <div style="clear:both;"></div>
-            </div>
-            
-            <div id="filtros_seccion">
-                <?php if (isset($str_info)) echo $str_info; ?>
-                <?php if (isset($str_errores)) echo $str_errores; ?>
-            </div>
-            <div id="filtros_seccion">
-                <form action="nuevo-cliente.php" method="post" id="form_tiendasvf_valores" name="form_tiedasvf_valores">
-                    <div class="login_form">
-                        <?php 
-                            echo $fM->get_input_hidden('id_usuario', $id_usuario);
-                            echo $fM->get_input_text('nombrecompleto_usuario', 'Nombre completo', $nombrecompleto_usuario, $arr_err);
-                            echo $fM->get_input_text('email_usuario', 'Email', $email_usuario, $arr_err);
-                            echo $fM->get_input_date('fecha_nacimiento', 'Fecha de nacimiento', $fecha_nacimiento, $arr_err);
-                            echo $fM->get_input_text('contrasenya_usuario', 'Contraseña', $contrasenya_usuario, $arr_err);
-                            echo $fM->get_input_text('telf_usuario', 'Telefono', $telf_usuario, $arr_err);
-                            echo $fM->get_input_text('nie_usuario', 'DNI / NIE', $nie_usuario, $arr_err);
-                        ?>
-                        <div style="float:right; margin:0.8em 0;">
-                            <input type="submit" style="float:none;" class="btn_aceptar" value="Aceptar" />
-                        </div>
-                        <div style="clear:both;"></div>
-                    </div>
-                </form>
-            </div>
-            
-        </div>
     </div>
-</div> -->
 </body>
 </html>
