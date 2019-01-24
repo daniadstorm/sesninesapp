1624e11fe328edd25f9526e300990bb8776228ae
Dani <danimartinezh>
2019-01-24 20:04:33 +0100

    :)

--- registro.php
+++ registro.php
@@ -12,41 +12,36 @@ $contrasenya_usuario = '';
 $str_error = '';
 
 //GET___________________________________________________________________________
-if (isset($_GET['unlogin'])) {
-    $uM->unlogin_usuario();
-}
+
 //GET___________________________________________________________________________
 
 //POST__________________________________________________________________________
-if (isset($_POST['nombre_usuario'])) { //si viene de submit de login
+if (isset($_POST['nombre_usuario']) && isset($_POST['contrasenya_usuario']) && isset($_POST['contrasenya_usuario_conf'])) { //si viene de submit de login
     
     $nombre_usuario = $_POST['nombre_usuario'];
     $contrasenya_usuario = $_POST['contrasenya_usuario'];
-    
-    $result_login = $uM->login_usuario($nombre_usuario, $contrasenya_usuario);
-    
-    if (strlen($result_login) > 1) {
-        $str_error = $result_login;
+
+    if($_POST['contrasenya_usuario']==$_POST['contrasenya_usuario_conf']){
+        
+    }else{
+        $str_error = 'Las contraseñas no coinciden';
     }
+    
 }
 
 //POST__________________________________________________________________________
 
 //CONTROL_______________________________________________________________________
 if (isset($_SESSION['id_tipo_usuario'])) { //si hay login
-    switch ($_SESSION['id_tipo_usuario']) {
-        default:
-        case USER:
-            header('Location: '.$ruta_inicio.'micuenta.php');
-            exit();
-        break;
-        case ADMIN:
-            header('Location: '.$ruta_inicio.'inicio-administrador.php');
-            exit();
-        break;
-    }
+    header('Location: '.$ruta_inicio.'index.php');
+    exit();
 }
 //CONTROL_______________________________________________________________________
+
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
 
 include_once('inc/cabecera.inc.php'); //cargando cabecera
 ?>
@@ -56,13 +51,15 @@ include_once('inc/cabecera.inc.php'); //cargando cabecera
         <div class="modallogin">
             <form action="" method="POST" class="frmlogin">
                 <div class="header">
-                    <h1 class="titulo">Inicia sesión en Sesnines</h1>
+                    <h1 class="titulo">Registrate en Sesnines</h1>
                 </div>
                 <hr>
                 <div class="body">
                     <?php echo ($str_error!='') ? $hM->get_alert_danger($str_error) : ''; ?>
-                    <?php echo $iM->get_input_text('nombre_usuario','','form-control frmlogin','Email'); ?>
-                    <?php echo $iM->get_input_text('contrasenya_usuario','','form-control frmpassword','Contraseña'); ?>
+                    <?php $nombre_usuario = (isset($_SESSION['frmdatosemail'])) ? $_SESSION['frmdatosemail'] : ''; ?>
+                    <?php echo $iM->get_input_text('nombre_usuario', $nombre_usuario, 'form-control frmlogin', 'Email'); ?>
+                    <?php echo $iM->get_input_text('contrasenya_usuario', '', 'form-control frmlogin', 'Contraseña'); ?>
+                    <?php echo $iM->get_input_text('contrasenya_usuario_conf', '', 'form-control frmlogin', 'Confirmar Contraseña'); ?>
                 </div>
                 <div class="d-flex flex-column">
                 <input type="submit" class="p-3 rounded border-0" value="Iniciar Sesión">
